813470ae17a9e14f91495a989c69091f2de990cd
OneKoala <iago.garcia.suarez@gmail.com>
2019-11-13 15:27:18 +0100

    README.md actualizado. Bot funcional pero sin comandos. Envía mensajes sólo al canal establecido para el bot.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # imonekbot
 Bot para el discord de los Come Eucaliptos
+
+Log:
+    - El bot se inicia y lee mensajes de todos los canales, pero sólo envía al canal especificado, en este momento es 'bot-commands'.
+    - Saludo de inicio de bot establecido. Cuando el bot se enciende enviará el saludo al canal del bot.
--- bot.py
+++ bot.py
@@ -9,21 +9,19 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
 bot = commands.Bot(command_prefix = '-')
-channel = None
+botChannelName = 'bot-commands'
 
-
-@bot.command(name='test')
-async def test(ctx):
-    
-    await channel.send('prueba')
+@bot.command(name='botpg')
+async def botpg(ctx):
+    await botChannel.send('prueba correcta')
 
 
 @bot.event
 async def on_ready():
-    #channel = discord.utils.get(ctx.guild.channels, name='bot-commands')
-    #channel.send('Listo para funcionar. Vuestros deseos son órdenes ( ° ͜ʖ °)')
-    for g in bot.get_all_channels():
-        print('Channel: {}\tID: {}'.format(g.name, g.id))
-    await bot.logout()
+    global botChannel
+    botChannel = discord.utils.get(bot.get_all_channels(), name=botChannelName)
+    await botChannel.send('Listo para funcionar. Vuestros deseos son órdenes ( ° ͜ʖ °)\nUtiliza -help para ver los comandos disponibles.')
+
+
 bot.run(TOKEN)
 
