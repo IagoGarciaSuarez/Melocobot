3c3412d5e929f5b5ca27a6a99e2e28f584d79533
OneKoala <iago.garcia.suarez@gmail.com>
2019-11-14 03:16:18 +0100

    Mejorado el comando de inicio. Comenzado el comando de creación de personaje y reestructurado el sistema de archivos.

--- Game_BotPG.py
+++ Game_BotPG.py
@@ -1,11 +1,32 @@
 import os
 
-def bienvenida(player_name):
-    if (os.path.isdir("Koala")):
-        return ('{} ha entrado en rpgbot. Elige qué personaje quieres utilizar:'.format(player_name))
-    else:
-        os.mkdir("Koala")
-        return ('¿Tu primera vez? Vamos a crear tu personaje:')
+prefix = '%'
 
-def creacionPersonaje(character_name):
-    f = open("{}.txt".format(character_name))
+def usrDir(destination):
+    return os.path.join('BotPG_Users', f'{destination}')
+
+def gameStart(player_name, user_id):
+    pjExiste = [1, f'''
+        {player_name} ya tienes un personaje creado. Opciones disponibles:
+            ·{prefix}botpg pjlist --> Muestra tus personajes creados.
+            ·{prefix}botpg select <personaje> --> Selecciona tu personaje.
+            ·{prefix}botpg edit <personaje> --> Editar a tu personaje.
+            ·{prefix}botpg reset <personaje> --> Reinicia tu personaje al nivel 0''']
+
+    pjNoExiste = [0, f'Bienvenido a BotPG, {player_name}, para crear un personaje nuevo escribe {prefix}botpg np.']
+    user_directory = usrDir(user_id)    
+    numOfChars = sum([len(files) for r, d, files in os.walk(usrDir(user_id))])
+
+    if (os.path.isdir(user_directory) and numOfChars):
+        return pjExiste
+
+    elif (not os.path.isdir(user_directory)):
+        os.mkdir(user_directory)
+        return pjNoExiste
+    
+    else:
+        return pjNoExiste
+
+def charCreation(char_name):
+    s = 'El personaje se llamará {}.'.format(char_name)
+    return s
